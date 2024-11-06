0abe49aaa9372698ffa38799153958d399d56e6c
akarmanov <mrmorris853@gmail.com>
2024-11-06 21:45:40 +0700

    merge develop

---
+++ apps/backend/src/main/java/net/akarmanov/projectplace/repos/MeetingRepository.java
@@ -0,0 +1,10 @@
+package net.akarmanov.projectplace.repos;
+
+import net.akarmanov.projectplace.domain.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface MeetingRepository extends JpaRepository<Meeting, UUID>, JpaSpecificationExecutor<Meeting> {
+}
