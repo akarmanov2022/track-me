414e0091943f64ba48c7f40d854c98a882c61928
NVA1337 <147712313+NVA1337@users.noreply.github.com>
2025-02-21 16:37:17 +0700

    добавил новый чекбокс (#120)
    * добавил новый чекбокс

--- apps/frontend/src/accets/stream-page/stream-page.js
+++ apps/frontend/src/accets/stream-page/stream-page.js
@@ -6,9 +6,13 @@ export default function Stream() {
   const [isVisible, setIsVisible] = useState(false);
   const [visibleCardsStart, setVisibleCardsStart] = useState(0);
   const [showCheckboxes, setShowCheckboxes] = useState(false);
-  const [data, setData] = useState({ content: [], page: {} }); // Состояние для хранения данных
+  const [showCheckboxes2, setShowCheckboxes2] = useState(false);
+  const [showCheckboxes3, setShowCheckboxes3] = useState(false);
   const [loading, setLoading] = useState(false); // Состояние для отслеживания загрузки
   const [error, setError] = useState(null); // Состояние для отслеживания ошибок
+  const [data, setData] = useState({ content: [], page: {} }); // Состояние для хранения данных
+
+  const numberOfCheckboxes = 9;
 
   // Функция для выполнения POST-запроса
   const fetchData = async () => {
@@ -16,14 +20,13 @@ export default function Stream() {
     setError(null);
   
   const token = localStorage.getItem("accessToken"); // Получаем токен из localStorage
-
     if (!token) {
       setError("Ошибка: отсутствует токен авторизации. Выполните вход.");
       setLoading(false);
       return;
     }
     try {
-      const response = await fetch('http://127.0.0.1:8080/api/v1/admin/streams?page=0&size=10', {
+       const response = await fetch('http://127.0.0.1:8080/api/v1/admin/streams?page=0&size=10', {
         method: 'POST',
       headers: {
         "Content-Type": "application/json",
@@ -53,10 +56,10 @@ export default function Stream() {
     }
   };
 
-  // Загружаем данные при монтировании компонента
   useEffect(() => {
     fetchData();
   }, []);
+
 
   // const data = {
   //   "content": [
@@ -170,63 +173,79 @@ export default function Stream() {
     setVisibleCardsStart(prev => Math.max(prev - 9, 0));
   };
 
+
   const handleShowCheckboxes = () => {
-    setShowCheckboxes(!showCheckboxes); // Переключаем видимость чекбоксов
+    setShowCheckboxes(!showCheckboxes);
   };
 
-  // const cards = Array.from({ length: 20 }, (_, index) => ({
-  //   id: index + 1,
-  //   title: `Карточка ${index + 1}`,
-  //   content: `Содержимое карточки ${index + 1}`,
-  // }));
+  const handleShowCheckboxes2 = () => {
+    setShowCheckboxes2(!showCheckboxes2);
+  };
+  const handleShowCheckboxes3 = () => {
+    setShowCheckboxes3(!showCheckboxes3);
+  };
+
 
   const visibleCards = cardd.slice(visibleCardsStart, visibleCardsStart + 9);
 
   // Пример данных для чекбоксов
-  const checkboxesData = Array.from({ length: 9 }, (_, index) => ({
+
+  const checkboxesData = Array.from({ length: numberOfCheckboxes }, (_, index) => ({
     id: `checkbox-${index + 1}`,
     label: `Чекбокс ${index + 1}`,
   }));
 
+  const checkboxesData2 = Array.from({ length: numberOfCheckboxes }, (_, index) => ({
+    id: `checkbox2-${index + 1}`,
+    label: `Чекбокс 2 ${index + 1}`,
+  }));
+  const checkboxesData3 = Array.from({ length: numberOfCheckboxes }, (_, index) => ({
+    id: `checkbox3-${index + 1}`,
+    label: `Чекбокс 3 ${index + 1}`,
+  }));
 
+if (true){
   if (loading) {
     return <div>Загрузка...</div>;
   }
 
   if (error) {
     return <div>Ошибка: {error}</div>;
-  }
+  }}
 
-  return (
-    <div className="Stream">
-      <header className="Stream-header">
-        <div className="Stream-header-cont">
-          <h1 className="Stream-title">Название</h1>
-          <div className="Stream-buttons">
-            <button className="Stream-butt">Администраторы</button>
-            <button className="Stream-butt">Трекеры</button>
-            <button className="Stream-butt">Все команды</button>
-            <Link to="/profile" className="Stream-pic"></Link>
+
+
+return (
+  <div className="Stream">
+    <header className="Stream-header">
+      <div className="Stream-header-cont">
+        <h1 className="Stream-title">Название</h1>
+        <div className="Stream-buttons">
+          <button className="Stream-butt">Администраторы</button>
+          <button className="Stream-butt">Трекеры</button>
+          <button className="Stream-butt">Все команды</button>
+          <Link to="/profile" className="Stream-pic"></Link>
+        </div>
+      </div>
+      <div className="Stream-header-bottom-cont">
+        <div className="Stream-search-cont">
+          <button onClick={handleClick} className="Stream-settings-pic"></button>
+          <div className="Stream-search-contcont">
+            <button className="Stream-settings-pic2"> </button>
+            <input type="search" placeholder="Найти" className="Stream-search" />
           </div>
         </div>
-        <div className="Stream-header-bottom-cont">
-          <div className="Stream-search-cont">
-            <button onClick={handleClick} className="Stream-settings-pic"></button>
-            <div className="Stream-search-contcont">
-              <button className="Stream-settings-pic2"> </button>
-              <input type="search" placeholder="Найти" className="Stream-search" />
+        <button className="Stream-butt">+ Создать карточку</button>
+      </div>
+      {isVisible && (
+        <div className="Stream-header-afterclick-cont">
+          <div className="Stream-header-afterclick-left">
+            <div className="Stream-header-afterclick-left-up">
+              <button className="Stream-header-chose-butt">Год [0]</button>
+              <button className="Stream-header-chose-butt">Рынок [0]</button>
             </div>
-          </div>
-          <button className="Stream-butt">+ Создать карточку</button>
-        </div>
-        {isVisible && (
-          <div className="Stream-header-afterclick-cont">
-            <div className="Stream-header-afterclick-left">
-              <div className="Stream-header-afterclick-left-up">
-                <button className="Stream-header-chose-butt">Год [0]</button>
-                <button className="Stream-header-chose-butt">Рынок [0]</button>
-              </div>
-              <div className="Stream-header-chosefrom-cont">
+            <div className="Stream-header-chosefrom-cont">
+              <div className="Stream-header-chosefrom-buttw">
                 <div className="Stream-header-chosefrom-butt">
                   <div className="Stream-header-chosefrom-butt-cont" onClick={handleShowCheckboxes}>
                     <b className="Stream-header-chosefrom-butt-label">Год</b>
@@ -243,53 +262,84 @@ export default function Stream() {
                     </div>
                   )}
                 </div>
-                <div className="Stream-header-chosefrom-butt2">
-                  <div className="Stream-header-chosefrom-butt-cont">
-                    <b className="Stream-header-chosefrom-butt-label">Рынок</b>
-                    <div className="Stream-header-chosefrom-butt-pic"></div>
-                  </div>
+              </div>
+              <div className="Stream-header-chosefrom-buttw">
+              <div className="Stream-header-chosefrom-butt2">
+                <div className="Stream-header-chosefrom-butt-cont" onClick={handleShowCheckboxes2}>
+                  <b className="Stream-header-chosefrom-butt-label">Рынок</b>
+                  <div className="Stream-header-chosefrom-butt-pic"></div>
                 </div>
+                {showCheckboxes2 && (
+                  <div className="Stream-header-checkboxes">
+                    {checkboxesData2.map((checkbox, index) => (
+                      <div key={checkbox.id} className={`Stream-header-checkbox ${index < 5 ? 'first-row' : 'second-row'}`}>
+                        <input type="checkbox" id={checkbox.id} />
+                        <label htmlFor={checkbox.id}>{checkbox.label}</label>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+              </div>
+              <div className="Stream-header-chosefrom-buttw">
+              <div className="Stream-header-chosefrom-butt2">
+                <div className="Stream-header-chosefrom-butt-cont" onClick={handleShowCheckboxes3}>
+                  <b className="Stream-header-chosefrom-butt-label">TRL</b>
+                  <div className="Stream-header-chosefrom-butt-pic"></div>
+                </div>
+                {showCheckboxes3 && (
+                  <div className="Stream-header-checkboxes">
+                    {checkboxesData3.map((checkbox, index) => (
+                      <div key={checkbox.id} className={`Stream-header-checkbox ${index < 5 ? 'first-row' : 'second-row'}`}>
+                        <input type="checkbox" id={checkbox.id} />
+                        <label htmlFor={checkbox.id}>{checkbox.label}</label>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
               </div>
             </div>
-            <div className="Stream-header-afterclick-right">
-              <button className="Stream-header-chose-butt2">Сбросить</button>
-              <button className="Stream-header-chose-butt">Применить</button>
-            </div>
           </div>
-        )}
-      </header>
-      <main className="Stream-main">
-        {visibleCards.map(card => (
-          <div key={card.id} className="Stream-card">
-            <div className="Stream-card-pic"></div>
-            <h1 className="Stream-card-headText">{card.title}</h1>
-            <div className="Stream-card-bodyText">{card.content}</div>
-          </div>
-        ))}
-      </main>
-      <footer className="Stream-footer">
-        <div className="Stream-footer-butts">
-          <div className="Stream-footer-p-butt-1">
-            {visibleCardsStart > 0 && (
-              <button onClick={handleShowPrevious} className="Stream-footer-button-1"></button>
-            )}
-          </div>
-          <div className="Stream-footer-p-butts">
-            {visibleCardsStart > 0 && (
-              <button onClick={handleShowPrevious} className="Stream-footer-button-2"></button>
-            )}
-            <button className="Stream-footer-button-3"></button>
-            {visibleCardsStart + 9 < cardd.length && (
-              <button onClick={handleShowMore} className="Stream-footer-button-4"></button>
-            )}
-          </div>
-          <div className="Stream-footer-p-butt-5">
-            {visibleCardsStart + 9 < cardd.length && (
-              <button onClick={handleShowMore} className="Stream-footer-button-5"></button>
-            )}
+          <div className="Stream-header-afterclick-right">
+            <button className="Stream-header-chose-butt2">Сбросить</button>
+            <button className="Stream-header-chose-butt">Применить</button>
           </div>
         </div>
-      </footer>
-    </div>
-  );
+      )}
+    </header>
+    <main className="Stream-main">
+      {visibleCards.map(card => (
+        <div key={card.id} className="Stream-card">
+          <div className="Stream-card-pic"></div>
+          <h1 className="Stream-card-headText">{card.title}</h1>
+          <div className="Stream-card-bodyText">{card.content}</div>
+        </div>
+      ))}
+    </main>
+    <footer className="Stream-footer">
+      <div className="Stream-footer-butts">
+        <div className="Stream-footer-p-butt-1">
+          {visibleCardsStart > 0 && (
+            <button onClick={handleShowPrevious} className="Stream-footer-button-1"></button>
+          )}
+        </div>
+        <div className="Stream-footer-p-butts">
+          {visibleCardsStart > 0 && (
+            <button onClick={handleShowPrevious} className="Stream-footer-button-2"></button>
+          )}
+          <button className="Stream-footer-button-3"></button>
+          {visibleCardsStart + 9 < cardd.length && (
+            <button onClick={handleShowMore} className="Stream-footer-button-4"></button>
+          )}
+        </div>
+        <div className="Stream-footer-p-butt-5">
+          {visibleCardsStart + 9 < cardd.length && (
+            <button onClick={handleShowMore} className="Stream-footer-button-5"></button>
+          )}
+        </div>
+      </div>
+    </footer>
+  </div>
+);
 }
