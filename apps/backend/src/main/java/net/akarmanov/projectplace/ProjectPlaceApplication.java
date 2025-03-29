aecd8e3311af69755833b819e4c7312e6022328b
Arseniy Karmanov <106430409+akarmanov2022@users.noreply.github.com>
2025-03-29 15:19:32 +0700

    Add SonarQube and JaCoCo plugins to build configuration and update CI pipeline (#211)
    * Add SonarQube and JaCoCo plugins to build configuration and update CI pipeline
    
    * Add logging on application startup and update SonarCloud action version
    
    * Add SonarCloud scan step to CI pipeline
    
    * Fix SonarCloud scan command in CI pipeline
    
    * Add SonarCloud analysis workflow and remove scan step from CI pipeline
    
    * Update CI pipeline to include JaCoCo and SonarCloud reporting in build step
    
    * Refactor CI configuration to enhance SonarCloud integration and update JaCoCo report settings
    
    * Refactor build configuration to apply Java plugin in subprojects and set Java toolchain version
    
    * Refactor project structure to update backend module path in settings.gradle
    
    * Add SonarQube plugin to build configuration and update PR checks for SonarCloud analysis
    
    * Enhance PR checks to trigger on opened, synchronized, and reopened events; separate SonarCloud scan into its own job
    
    * Update SonarCloud scan to use environment variable for SONAR_TOKEN
    
    * Remove unnecessary SonarCloud parameters from PR checks configuration
    
    * Configure SonarCloud properties and update scan command in CI workflow
    
    * Update SonarCloud scan to use secrets for project key and organization

--- apps/backend/src/main/java/net/akarmanov/projectplace/ProjectPlaceApplication.java
+++ apps/backend/src/main/java/net/akarmanov/projectplace/ProjectPlaceApplication.java
@@ -1,13 +1,24 @@
 package net.akarmanov.projectplace;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class ProjectPlaceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProjectPlaceApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner commandLineRunner() {
+    return args -> {
+      log.info("Сервер запущен");
+    };
   }
 
 }
