3a9098f61126f47dcbfc4c24ddd9d0a1095fe4bb
Arseniy Karmanov <106430409+akarmanov2022@users.noreply.github.com>
2025-04-28 22:39:18 +0700

    bugfix/registration-mail (#236)
    * Remove 'war' plugin and enhance Docker security.
    
    The 'war' plugin was removed as it is unnecessary for the current modules. Dockerfiles were updated to include a non-root user for improved container security, and the app jar file ownership was adjusted accordingly. Additionally, the application.yaml now excludes the /api/csrf path from OpenAPI documentation.
    
    * Refactor email template path in DefaultMailService.
    
    Updated the email confirmation template path to reflect the new location by removing the `mail/` directory. This ensures consistency and correct reference to the template in the service.
    
    * Update registration confirmation endpoint paths
    
    Aligned `registration-confirmation-endpoint` for both SSO clients and authorization server configurations. Introduced profile-specific configuration for the development environment to ensure environment-specific behavior.

--- services/trackme-sso/src/main/java/net/akarmanov/projectplace/sso/services/impl/DefaultMailService.java
+++ services/trackme-sso/src/main/java/net/akarmanov/projectplace/sso/services/impl/DefaultMailService.java
@@ -20,7 +20,7 @@ import static java.util.concurrent.CompletableFuture.runAsync;
 @RequiredArgsConstructor
 public class DefaultMailService implements MailService {
 
-  public static final String CONFIRMATION_HTML = "mail/email-confirmation.html";
+  public static final String CONFIRMATION_HTML = "email-confirmation.html";
 
   private final JavaMailSender mailSender;
 
