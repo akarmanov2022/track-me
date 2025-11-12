f33d3afb34fa7d251143b6cd9a657a35b6db9c5c
popovam <kemanton22@mail.ru>
2025-11-12 19:05:28 +0700

    [SBI-462] Регистрация API телеграм бота вынесена в отдельный метод

--- services/telegram-service/src/main/java/net/trackme/telegramservice/TelegramServiceApplication.java
+++ services/telegram-service/src/main/java/net/trackme/telegramservice/TelegramServiceApplication.java
@@ -1,25 +1,11 @@
 package net.trackme.telegramservice;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import net.trackme.telegramservice.configuration.NotificationBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Slf4j
-@AllArgsConstructor
 @SpringBootApplication
 public class TelegramServiceApplication {
-    public static void main(String[] args) throws TelegramApiException {
-        var context = SpringApplication.run(TelegramServiceApplication.class, args);
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        try {
-            telegramBotsApi.registerBot(context.getBean("notificationBot", NotificationBot.class));
-        } catch (TelegramApiException e) {
-            log.error("Telegram bot API could not register notification bot.");
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramServiceApplication.class, args);
     }
 }
