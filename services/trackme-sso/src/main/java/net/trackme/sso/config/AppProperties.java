96fc449068d87fc0105d78eac9c9565b72339259
popovam <kemanton22@mail.ru>
2025-11-19 22:43:31 +0700

    [SBI-462] Добавлена ссылка на телеграм бота в сообщении на почте при регистрации

--- services/trackme-sso/src/main/java/net/trackme/sso/config/AppProperties.java
+++ services/trackme-sso/src/main/java/net/trackme/sso/config/AppProperties.java
@@ -19,6 +19,9 @@ public class AppProperties {
   @NotBlank(message = "URL cannot be blank")
   private String apiUrl;
 
+  @NotBlank(message = "Bot username cannot be blank")
+  private String botUsername;
+
   private MailProperties mail = new MailProperties();
 
   @Data
--- services/trackme-sso/src/main/java/net/trackme/sso/services/impl/DefaultRegistrationService.java
+++ services/trackme-sso/src/main/java/net/trackme/sso/services/impl/DefaultRegistrationService.java
@@ -63,7 +63,8 @@ public class DefaultRegistrationService implements RegistrationService {
             "email", requestDto.email(),
             "appName", appProperties.getMail().getSubject(),
             "supportEmail", appProperties.getMail().getFrom(),
-                "confirmationLink", getConfirmationLink(tokenHash)));
+                "confirmationLink", getConfirmationLink(tokenHash),
+                "notificationBotLink", getNotificationBotLink()));
   }
 
   @Override
@@ -81,5 +82,9 @@ public class DefaultRegistrationService implements RegistrationService {
     return UriComponentsBuilder.fromUriString(httpUrl, UriComponentsBuilder.ParserType.WHAT_WG)
         .queryParam("token", token)
         .build().toUriString();
+  }
+
+  private String getNotificationBotLink() {
+    return String.format("https://t.me/%s?start_" + System.currentTimeMillis(), appProperties.getBotUsername());
   }
 }
