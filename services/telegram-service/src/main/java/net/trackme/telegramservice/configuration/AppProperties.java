3bc7be26e2637c6561f190968c9301ec4c5c3b1b
popovam <kemanton22@mail.ru>
2025-11-24 04:33:52 +0700

    [SBI-462] Исправлены миграции

--- services/telegram-service/src/main/java/net/trackme/telegramservice/configuration/AppProperties.java
+++ services/telegram-service/src/main/java/net/trackme/telegramservice/configuration/AppProperties.java
@@ -4,33 +4,33 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/**
+/**.
  * Настройки приложения
  */
 @Data
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
 
-    /**
+    /**.
      * Настройки телеграм бота
      */
     private TelegramBotProperties telegramBotProperties = new TelegramBotProperties();
 
-    /**
+    /**.
      * Настройки телеграм бота
      */
     @Data
     public static class TelegramBotProperties {
-        /**
+        /**.
          * Имя бота
          */
-        @NotBlank(message="Bot username cannot be blank")
+        @NotBlank(message = "Bot username cannot be blank")
         private String botUsername;
 
-        /**
+        /**.
          * Токен бота
          */
-        @NotBlank(message="Bot token cannot be blank")
+        @NotBlank(message = "Bot token cannot be blank")
         private String botToken;
     }
 }
