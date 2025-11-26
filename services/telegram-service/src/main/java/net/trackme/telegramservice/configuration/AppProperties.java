f796929d31e714aee0d6dce1045101bb5414eb76
popovam <kemanton22@mail.ru>
2025-11-26 21:42:36 +0700

    [SBI-577] Исправления

--- services/telegram-service/src/main/java/net/trackme/telegramservice/configuration/AppProperties.java
+++ services/telegram-service/src/main/java/net/trackme/telegramservice/configuration/AppProperties.java
@@ -4,31 +4,31 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/**.
- * Настройки приложения
+/**
+ * Настройки приложения.
  */
 @Data
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
 
-    /**.
-     * Настройки телеграм бота
+    /**
+     * Настройки телеграм бота.
      */
     private TelegramBotProperties telegramBotProperties = new TelegramBotProperties();
 
-    /**.
-     * Настройки телеграм бота
+    /**
+     * Настройки телеграм бота.
      */
     @Data
     public static class TelegramBotProperties {
-        /**.
-         * Имя бота
+        /**
+         * Имя бота.
          */
         @NotBlank(message = "Bot username cannot be blank")
         private String botUsername;
 
-        /**.
-         * Токен бота
+        /**
+         * Токен бота.
          */
         @NotBlank(message = "Bot token cannot be blank")
         private String botToken;
