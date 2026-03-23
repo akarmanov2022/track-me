e2c73bdbe415dd6e59aa4053a1eca4c80792fe44
emma klatscht <51996726+tr1ppy9in3@users.noreply.github.com>
2026-03-23 13:39:58 +0700

    Revert "[SBI-725] Ограничение для создания более одной встречи в день. (#437)" (#442)
    This reverts commit a64f1cb13ac4bf43c4b65c3c1573b33dbf436bbc.

--- services/meeting-service/src/main/java/net/trackme/meetingservice/dao/MeetingRepository.java
+++ services/meeting-service/src/main/java/net/trackme/meetingservice/dao/MeetingRepository.java
@@ -5,8 +5,6 @@ import net.trackme.meetingservice.entities.MeetingStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -19,17 +17,4 @@ public interface MeetingRepository extends JpaRepository<Meeting, UUID>, JpaSpec
 
     List<Meeting> findByStatusAndStartDateBefore(MeetingStatus status, OffsetDateTime before,
                                                  Pageable pageable);
-
-    boolean existsByTeamCardIdAndStartDateGreaterThanEqualAndStartDateLessThan(
-            UUID teamCardId,
-            OffsetDateTime from,
-            OffsetDateTime to
-    );
-
-    boolean existsByTeamCardIdAndStartDateGreaterThanEqualAndStartDateLessThanAndIdNot(
-            UUID teamCardId,
-            OffsetDateTime from,
-            OffsetDateTime to,
-            UUID excludeId
-    );
 }
