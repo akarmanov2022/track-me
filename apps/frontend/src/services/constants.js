81286b4a506195f6d69f6b22f951d2e37a3c448d
Ivan Lifanov <letalbark@gmail.com>
2026-03-24 19:42:09 +0700

    add meeting backend url

--- apps/frontend/src/services/constants.js
+++ apps/frontend/src/services/constants.js
@@ -1,6 +1,7 @@
 export const backendURL = process.env.REACT_APP_BACKEND_URI || "http://localhost:8081";
 export const backendURLSSO = backendURL + "/sso";
 export const backendURLBackend = backendURL + "/backend";
+export const backendURLMeeting = backendURL + "/meeting";
 export const superadminRoleName = "SUPER_ADMIN";
 export const adminRoleName = "ADMIN";
 export const trackerRoleName = "TRACKER";
