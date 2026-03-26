d3b3c28cfca8063591105fd1240452322e17ccad
Ivan Lifanov <letalbark@gmail.com>
2026-03-26 07:58:54 +0100

    fix colors for registration success on mobile and change the message to specify email (#448)

--- services/trackme-sso/frontend/src/assets/registration-success/RegistrationSuccess.js
+++ services/trackme-sso/frontend/src/assets/registration-success/RegistrationSuccess.js
@@ -6,7 +6,7 @@ const RegistrationSuccess = () => {
         <div className="register-container">
             <div className="register-box">
                 <h1 className="register-title">Регистрация успешна!</h1>
-                <p>На вашу почту отправлено сообщение с ссылкой для подтверждения.</p>
+                <p>На вашу почту отправлено сообщение с ссылкой для подтверждения почты.</p>
             </div>
         </div>
     );
