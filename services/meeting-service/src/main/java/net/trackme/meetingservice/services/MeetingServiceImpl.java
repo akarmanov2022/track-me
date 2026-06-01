813880c9c6e0a7a7cd9cc1263dc5d9a63aff2fcc
Dmitry <dimka182005@gmail.com>
2026-06-01 16:19:23 +0700

    feat: суперадмин может менять статус встречи и обновлять рейтинг команды.

--- services/meeting-service/src/main/java/net/trackme/meetingservice/services/MeetingServiceImpl.java
+++ services/meeting-service/src/main/java/net/trackme/meetingservice/services/MeetingServiceImpl.java
@@ -272,7 +272,8 @@ public class MeetingServiceImpl implements MeetingService {
         recalculateTasksChain(teamCardId);
 
         // ИЗМЕНЕНО: отправляем событие при изменении статуса ИЛИ teamStatus
-        if (oldStatus != meeting.getStatus() || !Objects.equals(oldTeamStatus, meeting.getTeamStatus())) {
+        if (oldStatus != meeting.getStatus() || 
+        !Objects.equals(oldTeamStatus, meeting.getTeamStatus())) {
             sendMeetingUpdatedEvent(savedMeeting, oldStatus);
         }
 
